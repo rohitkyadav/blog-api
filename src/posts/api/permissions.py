8501ea302aff29ce84123fb845cc5d90355526a7
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2016-11-21 16:40:30 +0530

    main project

--- src/posts/api/permissions.py
+++ src/posts/api/permissions.py
@@ -1,14 +1,13 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-
 class IsOwnerOrReadOnly(BasePermission):
 	message = 'You must be the owner of the object.'
 	#my_safe_method = ['GET', 'PUT']
 
 	#def has_permission(self, request, view):
-	#	if request.method in self.my_safe_method:
-	#		return True
-	#	return False
+		#if request.method in self.my_safe_method:
+			#return True
+		#return False
 
 
 	def has_object_permission(self, request, view, obj):
