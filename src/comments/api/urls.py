673d918406dfa9604a5ee189702f9418a213c6bb
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2016-11-24 23:14:22 +0530

    main project

--- src/comments/api/urls.py
+++ src/comments/api/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
 	url(r'^$', CommentListAPIView.as_view(), name='list'),
 	url(r'^create/$', CommentCreateAPIView.as_view(), name='create'),
-    url(r'^(?P<pk>\d+)/$', CommentDetailAPIView.as_view(), name='thread'),
-    #url(r'^(?P<pk>\d+)/edit/$', CommentEditAPIView.as_view(), name='edit'),
-#    url(r'^(?P<id>\d+)/delete/$', comment_delete, name='delete'),
+	url(r'^(?P<pk>\d+)/$', CommentDetailAPIView.as_view(), name='thread'),
+	#url(r'^(?P<pk>\d+)/edit/$', CommentEditAPIView.as_view(), name='edit'),
+	#url(r'^(?P<id>\d+)/delete/$', comment_delete, name='delete'),
 ]
