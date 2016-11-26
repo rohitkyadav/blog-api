6775ed4292d8e6ded076d9502f0464348470a871
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2016-11-27 01:59:39 +0530

    main project

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -24,19 +24,18 @@ from rest_framework_jwt.views import obtain_jwt_token
 from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    
-    url(r'^admin/', admin.site.urls),
-    url(r'^comments/', include("comments.urls", namespace='comments')),
-    
-    url(r'^register/', register_view, name='register'),
-    url(r'^login/', login_view, name='login'),
-    url(r'^logout/', logout_view, name='logout'),
-    url(r'^', include("posts.urls", namespace='posts')),
-    url(r'^api/auth/token/', obtain_jwt_token),
-    url(r'^api/users/', include("accounts.api.urls", namespace='users-api')),
-    url(r'^api/comments/', include("comments.api.urls", namespace='comments-api')),
-    url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
+	url(r'^admin/', admin.site.urls),
+	url(r'^comments/', include("comments.urls", namespace='comments')),
+
+	url(r'^register/', register_view, name='register'),
+	url(r'^login/', login_view, name='login'),
+	url(r'^logout/', logout_view, name='logout'),
+	url(r'^', include("posts.urls", namespace='posts')),
+	url(r'^api/auth/token/', obtain_jwt_token),
+	url(r'^api/users/', include("accounts.api.urls", namespace='users-api')),
+	url(r'^api/comments/', include("comments.api.urls", namespace='comments-api')),
+	url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
+	#url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
